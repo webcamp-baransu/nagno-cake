fcf7781c1954e73288f77b389a3f76c63866485c
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-21 09:15:25 +0000

    Connection_admin/customer_index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
@@ -7,7 +8,14 @@ class Admin::CustomersController < ApplicationController
 
   def edit
   end
-  
+
   def update
   end
+
+  private
+
+  def user_params
+    params.require(:customer).permit(:last_name, :first_name, :email, :id, :is_deleted)
+  end
+
 end
