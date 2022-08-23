c91f5e2b37662cc2b1176cc9f7c6fbad0040ed8c
EC2 Default User <ec2-user@ip-172-31-43-214.us-west-2.compute.internal>
2022-08-23 05:13:24 +0000

    customers-show

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,23 +1,23 @@
 class Public::CustomersController < ApplicationController
   def show
-    
+
   end
 
   def edit
-    
+    @customer = current_customer
   end
 
   def unsubscribe
-    
+
   end
-  
+
   def update
-    
-  end 
-  
-  def withdrawa
-    
+
   end
-  
-  
+
+  def withdrawa
+
+  end
+
+
 end
