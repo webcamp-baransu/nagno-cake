acff0cb2bf6d3c645b3df12ab6790cea03b12f8c
shino3769 <108673696+shino3769@users.noreply.github.com>
2022-08-23 04:57:36 +0000

    shipping_addresses

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,5 +1,8 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
+    @shipping_address = ShippingAddress.new
+    @customer = current_customer
+    @shipping_addresses = @customer.shipping_addresses
   end
 
   def edit
