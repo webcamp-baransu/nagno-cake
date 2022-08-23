4451bcc3b53c262d8d6723053dd27f506f1ffd87
EC2 Default User <ec2-user@ip-172-31-43-214.us-west-2.compute.internal>
2022-08-23 09:49:31 +0000

    customers_show

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,26 +8,27 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
-
+   @customer = current_customer
   end
 
   def update
-    @customers_url = customer.find(params[:id])
+    @customer = current_customer
 
       if @customer.update(customer_params)
       flash[:notice] = "successfully"
-      redirect_to customers_path(@customer.id)
+      redirect_to customers_path
 
       else
       render :show
-
       end
-
   end
 
   def withdrawa
-
+   @customer = current_customer
   end
 
-
+  private
+  def customer_params
+    params.require(:customer).permit( :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
 end
