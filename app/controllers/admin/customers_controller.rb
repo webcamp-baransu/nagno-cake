25ad8509abd7f0410348ebb03761bbd230f7eea4
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-24 05:05:54 +0000

    admin_customers/show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,8 +15,8 @@ class Admin::CustomersController < ApplicationController
 
   private
 
-  def customers_params
-    params.require(:customer).permit(:last_name, :first_name, :email, :id, :is_deleted, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number)
+  def customer_params
+    params.require(:customer).permit(:telephone_number, :last_name, :first_name, :email, :id, :is_deleted, :last_name_kana, :first_name_kana, :postal_code, :address)
   end
 
 end
