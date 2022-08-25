2baa01958fc4df69446f5364009e5035a08b00c8
shino3769 <108673696+shino3769@users.noreply.github.com>
2022-08-25 13:23:25 +0000

    order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,8 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order=Order.new
+    @cart_items = CartItem.all
+    @total = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.sum_of_price }+800
   end
 
   def log
