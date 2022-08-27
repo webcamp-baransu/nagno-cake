afb71679d74d03dcfa28683b73e385cd017f7e03
shino3769 <108673696+shino3769@users.noreply.github.com>
2022-08-27 11:22:22 +0000

    cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -25,25 +25,27 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
-  def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_items = CartItem.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        
+def create
+    @cart_item_check = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
+    if @cart_item_check
+      @cart_item_check.amount += params[:cart_item][:amount].to_i
+      @cart_item_check.save
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        redirect_back(fallback_location: root_path)
       end
     end
-    @cart_item.save
-    redirect_to cart_items_path
-  end
+end
+
 
   private
 
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
-
 end
