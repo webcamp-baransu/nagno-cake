add48aecb68a5e1cc49ec76cac6227b2de087d4a
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-27 00:33:56 +0000

    [finish]admin_order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,12 +3,12 @@ class Admin::OrderDetailsController < ApplicationController
      @order_detail = OrderDetail.find(params[:id])
   if @order_detail.update(order_detail_params)
    flash[:notice] = "対応ステータスを更新しました"
-   redirect_to admin_order_detail_path(@order_detail)
+   redirect_to admin_order_path
   else
    render :show, alert: "対応ステータスを更新できませんでした"
   end
  end
- 
+
  private
 
   def order_detail_params
